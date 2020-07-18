17aaddd006443455567ce81c590e49f1e82ca296
Saggre <sakri.koskimies@hotmail.com>
2020-07-19 00:04:42 +0300

    Develop - wrong broadcast direction

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,309 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <WiFi101.h>
+#include <WiFiUdp.h>
+#include <HX711_ADC.h>
+#include "secret.h"
 
- // the setup function runs once when you press reset or power the board
-void setup() {
+// Pins:
+const int HX711_dout = 6; //mcu > HX711 dout pin
+const int HX711_sck = 4;  //mcu > HX711 sck pin
+
+// WIFI
+const int port = 21212;
+char ssid[] = SECRET_SSID;   // your network SSID (name)
+char pass[] = SECRET_PASS;   // your network password (use for WPA, or use as key for WEP)
+int status = WL_IDLE_STATUS; // the WiFi radio's status
+WiFiServer server(port);
+WiFiUDP udp;
+IPAddress broadcastIp;
+bool alreadyConnected = false; // whether or not the client was connected previously
+
+//HX711 constructor:
+HX711_ADC LoadCell(HX711_dout, HX711_sck);
+
+long t;
+float calibrationValue = 380.02;
+
+void calibrate()
+{
+  Serial.println("***");
+  Serial.println("Start calibration:");
+  Serial.println("Place the load cell an a level stable surface.");
+  Serial.println("Remove any load applied to the load cell.");
+  Serial.println("Send 't' from serial monitor to set the tare offset.");
+
+  boolean _resume = false;
+  while (_resume == false)
+  {
+    LoadCell.update();
+    if (Serial.available() > 0)
+    {
+      if (Serial.available() > 0)
+      {
+        float i;
+        char inByte = Serial.read();
+        if (inByte == 't')
+          LoadCell.tareNoDelay();
+      }
+    }
+    if (LoadCell.getTareStatus() == true)
+    {
+      Serial.println("Tare complete");
+      _resume = true;
+    }
+  }
+
+  Serial.println("Now, place your known mass on the loadcell.");
+  Serial.println("Then send the weight of this mass (i.e. 100.0) from serial monitor.");
+
+  float known_mass = 0;
+  _resume = false;
+  while (_resume == false)
+  {
+    LoadCell.update();
+    if (Serial.available() > 0)
+    {
+      known_mass = Serial.parseFloat();
+      if (known_mass != 0)
+      {
+        Serial.print("Known mass is: ");
+        Serial.println(known_mass);
+        _resume = true;
+      }
+    }
+  }
+
+  LoadCell.refreshDataSet();                                          //refresh the dataset to be sure that the known mass is measured correct
+  float newCalibrationValue = LoadCell.getNewCalibration(known_mass); //get the new calibration value
+
+  Serial.print("New calibration value has been set to: ");
+  Serial.print(newCalibrationValue);
+  Serial.println(", use this as calibration value (calFactor) in your project sketch.");
+  Serial.print("Save this value to a variable y/n?");
+
+  _resume = false;
+  while (_resume == false)
+  {
+    if (Serial.available() > 0)
+    {
+      char inByte = Serial.read();
+      if (inByte == 'y')
+      {
+        calibrationValue = newCalibrationValue;
+        Serial.print("Value ");
+        Serial.print(calibrationValue);
+        Serial.print(" saved to a variable");
+        LoadCell.setCalFactor(calibrationValue);
+        _resume = true;
+      }
+      else if (inByte == 'n')
+      {
+        Serial.println("Value not saved");
+        _resume = true;
+      }
+    }
+  }
+
+  Serial.println("End calibration");
+  Serial.println("***");
+  Serial.println("To re-calibrate, send 'r' from serial monitor.");
+  Serial.println("For manual edit of the calibration value, send 'c' from serial monitor.");
+  Serial.println("***");
+}
+
+void printMacAddress(byte mac[])
+{
+  for (int i = 5; i >= 0; i--)
+  {
+    if (mac[i] < 16)
+    {
+      Serial.print("0");
+    }
+    Serial.print(mac[i], HEX);
+    if (i > 0)
+    {
+      Serial.print(":");
+    }
+  }
+  Serial.println();
+}
+
+void printCurrentNet()
+{
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print the MAC address of the router you're attached to:
+  byte bssid[6];
+  WiFi.BSSID(bssid);
+  Serial.print("BSSID: ");
+  printMacAddress(bssid);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.println(rssi);
+
+  // print the encryption type:
+  byte encryption = WiFi.encryptionType();
+  Serial.print("Encryption Type:");
+  Serial.println(encryption, HEX);
+  Serial.println();
+}
+
+void printWiFiData()
+{
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+  Serial.println(ip);
+
+  // print your MAC address:
+  byte mac[6];
+  WiFi.macAddress(mac);
+  Serial.print("MAC address: ");
+  printMacAddress(mac);
+}
+
+// the setup function runs once when you press reset or power the board
+void setup()
+{
 
   Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
+  Serial.println("\nStarting...");
+
+  LoadCell.begin();
+
+  long stabilizingtime = 2000; // preciscion right after power-up can be improved by adding a few seconds of stabilizing time
+  boolean _tare = true;        //set this to false if you don't want tare to be performed in the next step
+  LoadCell.start(stabilizingtime, _tare);
+  if (LoadCell.getTareTimeoutFlag())
+  {
+    Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
+    while (1)
+      ;
   }
+  else
+  {
+    LoadCell.setCalFactor(calibrationValue); // set calibration value (float)
+    Serial.println("Startup is complete");
+  }
+
+  //-------------------------------------------WIFI---------------------------------------------
 
   // check for the presence of the shield:
-  if (WiFi.status() == WL_NO_SHIELD) {
+  if (WiFi.status() == WL_NO_SHIELD)
+  {
     Serial.println("NOT PRESENT");
-    return; // don't continue
+    while (true)
+      ; // don't continue
   }
 
-  String firmware = WiFi.firmwareVersion();
-  Serial.print(firmware);
+  // attempt to connect to WiFi network:
+  while (status != WL_CONNECTED)
+  {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network:
+    status = WiFi.begin(ssid, pass);
 
-  // initialize digital pin 13 as an output.
-  pinMode(13, OUTPUT);
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+
+  Serial.print("Starting server\n");
+  // start the server:
+  server.begin();
+  udp.begin(port);
+
+  Serial.print("You're connected to the network\n");
+  printCurrentNet();
+  printWiFiData();
+  delay(1000);
 }
 
 // the loop function runs over and over again forever
-void loop() {
-  String firmware = WiFi.firmwareVersion();
-  Serial.print(firmware);
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);              // wait for a second
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);              // wait for a second
+void loop()
+{
+
+  // wait for a new client:
+  WiFiClient client = server.available();
+
+  if (client)
+  {
+    if (!alreadyConnected)
+    {
+      // clead out the input buffer:
+      client.flush();
+      Serial.println("We have a new client");
+      client.println("Hello, client!");
+      alreadyConnected = true;
+    }
+
+    if (client.available() > 0)
+    {
+      // read the bytes incoming from the client:
+      char thisChar = client.read();
+      // echo the bytes back to the client:
+      server.write(thisChar);
+      // echo the bytes to the server as well:
+      Serial.write(thisChar);
+    }
+  }
+  else if (!alreadyConnected)
+  {
+    // Send broadcast until a device connects to us
+    // When it has connected once, it must have this ip
+    Serial.print("Sending broadcast\n");
+    udp.beginPacket(IPAddress(255, 255, 255, 255), port);
+    char str[] = "vaaka";
+    udp.write(str);
+    udp.endPacket();
+
+    delay(500);
+  }
+
+  return;
+  //-----------------
+
+  static boolean newDataReady = 0;
+  const int serialPrintInterval = 0; //increase value to slow down serial print activity
+
+  // check for new data/start next conversion:
+  if (LoadCell.update())
+    newDataReady = true;
+
+  // get smoothed value from the dataset:
+  if (newDataReady)
+  {
+    if (millis() > t + serialPrintInterval)
+    {
+      float i = LoadCell.getData();
+      //Serial.print("Load_cell output val: ");
+      //Serial.println(i);
+      newDataReady = 0;
+      t = millis();
+    }
+  }
+
+  // receive command from serial terminal
+  if (Serial.available() > 0)
+  {
+    char inByte = Serial.read();
+    if (inByte == 't')
+      LoadCell.tareNoDelay(); //tare
+    else if (inByte == 'r')
+      calibrate(); //calibrate
+    else if (inByte == 'c')
+      calibrate();
+  }
+
+  // check if last tare operation is complete
+  if (LoadCell.getTareStatus() == true)
+  {
+    Serial.println("Tare complete");
+  }
 }
---
+++ src/secret.h
@@ -0,0 +1,3 @@
+// WIFI Conn info
+#define SECRET_SSID "Surveillance Van"
+#define SECRET_PASS "5YcosvNjqtm4HHxA1ndE"
